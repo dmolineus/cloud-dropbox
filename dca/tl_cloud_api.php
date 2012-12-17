cc5b380bf507fb91606910925ac8554386f7d406
David Molineus <support@marmalade.de>
2012-12-17 12:48:51 +0100

    use metamodels for tl_cloud_api palettes and subpalettes

--- dca/tl_cloud_api.php
+++ dca/tl_cloud_api.php
@@ -11,9 +11,13 @@
  * @copyright Copyright 2012 David Molineus netzmacht creative 
  **/
 
-$GLOBALS['TL_DCA']['tl_cloud_api']['palettes']['dropbox'] = '{connection_legend},title,enabled;{folder_legend:hide},mountedFolders;{custom_legend:hide},useCustomApp';
-$GLOBALS['TL_DCA']['tl_cloud_api']['customSubPalettes']['dropbox'] = array
+$GLOBALS['TL_DCA']['tl_cloud_api']['metapalettes']['dropbox extends _base_'] = array
 (
-	'enabled' => 'oAuthClass,accessToken',
-	'useCustomApp' => 'appKey,appSecret,root',
+	'custom'		=> array(':hide', 'useCustomApp'),
+);
+
+$GLOBALS['TL_DCA']['tl_cloud_api']['cloudapi_metasubselectpalettes']['dropbox'] = array
+(
+	'enabled' => array('oAuthClass', 'accessToken'),
+	'useCustomApp' => array('appKey','appSecret,root'),
 );
